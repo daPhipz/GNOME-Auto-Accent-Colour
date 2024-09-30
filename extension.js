d2e36a6798ae6777fd9d05a766d261762e3c99b8
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-30 20:52:14 +0100

    Extension now fetches accent colour.

--- extension.js
+++ extension.js
@@ -1,13 +1,16 @@
 import St from 'gi://St'
+import Gio from 'gi://Gio'
 import * as Main from 'resource:///org/gnome/shell/ui/main.js'
 import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js'
 
+const INTERFACE_SCHEMA = 'org.gnome.desktop.interface'
+
 export default class AutoAccentColourExtension extends Extension {
     enable() {
-        let gsettings = new Gio.Settings('org.gnome.desktop.interface');
-        let accent = gsettings.get_string('accent-color')
+        const _gsettings = new Gio.Settings({ schema: INTERFACE_SCHEMA });
+        const _accent = _gsettings.get_string('accent-color')
 
-        Main.notify(accent)
+        Main.notify('Accent Color', _accent)
     }
 
     disable() {
