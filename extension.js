146b8f85b01304fe16e4b55c41499b28b2ff2f7e
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-09-30 19:05:13 +0100

    Initial commit

---
+++ extension.js
@@ -0,0 +1,12 @@
+import St from 'gi://St'
+import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js'
+
+export default class AutoAccentColourExtension extends Extension {
+    enable() {
+
+    }
+
+    disable() {
+
+    }
+}
